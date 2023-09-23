69f936ddfb305c2cbd0b49be796f8d42d9afcbbb
me_madness <p_rangel@abv.bg>
2023-09-23 21:20:43 +0200

    07.done

--- Exercise/07. water_overflow.py
+++ Exercise/07. water_overflow.py
@@ -1,13 +1,35 @@
 n = int(input())
 count_of_litters = 0
-
-
-for litter in range(1, n + 1):
+capacity = 255
+for litter in range(n):
     litters_income = int(input())
-    count_of_litters += litters_income
-    if count_of_litters >= 255:
+    if capacity - litters_income < 0:
         print("Insufficient capacity!")
-        print(count_of_litters - litters_income)
-        break
+        continue
+    capacity -= litters_income
+    count_of_litters += litters_income
+print(count_of_litters)
 
-print(count_of_litters) 
+
+# for litter in range(n):
+#     litters_income = int(input())
+#     if capacity - litters_income > 0:
+#         count_of_litters += litters_income
+#         if count_of_litters > capacity:
+#             print("Insufficient capacity!")
+#             count_of_litters -= litters_income
+#             continue
+#     print(count_of_litters) 
+
+# number_of_lines = int(input())
+# water_counter = 0
+# tank_capacity = 255
+# for line in range(number_of_lines):
+#     liters_of_water = int(input())
+#     if tank_capacity - liters_of_water < 0:  # no enough capacity
+#         print("Insufficient capacity!")
+#         continue
+#     tank_capacity -= liters_of_water
+#     water_counter += liters_of_water
+# print(water_counter)
+
