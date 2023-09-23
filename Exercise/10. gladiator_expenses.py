89455ae0eedf04a8f2e91e441512502fb2102ba7
me_madness <p_rangel@abv.bg>
2023-09-23 23:48:03 +0200

    10. Input it's finish

---
+++ Exercise/10. gladiator_expenses.py
@@ -0,0 +1,9 @@
+lost_fight_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+expenses = 0
+
+
+print(f"Gladiator expenses: {expenses} aureus")
