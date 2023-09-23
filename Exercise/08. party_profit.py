58ffafeb35c47af06d75ff420994bd05d9356a57
me_madness <p_rangel@abv.bg>
2023-09-23 22:23:47 +0200

    08. Ready man

---
+++ Exercise/08. party_profit.py
@@ -0,0 +1,18 @@
+group_size = int(input())
+days = int(input())
+income_day_coins = (50 - (group_size * 2)) * days
+
+for day_count in range(1, days + 1):
+    if day_count % 3 == 0:
+        income_day_coins -= group_size * 3
+    if day_count % 5 == 0:
+        income_day_coins += group_size * 20
+        if day_count % 3 == 0:
+            income_day_coins -= group_size * 2
+    if day_count % 10 == 0:        
+        group_size -= 2
+    if day_count % 15 == 0:
+        group_size += 5
+        
+coins = int(income_day_coins / group_size)      
+print(f"{group_size} companions received {coins} coins each.")
