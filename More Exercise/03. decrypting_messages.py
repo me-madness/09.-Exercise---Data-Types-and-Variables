717d8878aad555dfcc1940bf5c6d7426d596f497
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-24 21:03:52 +0200

    03. are done

--- More Exercise/03. decrypting_messages.py
+++ More Exercise/03. decrypting_messages.py
@@ -3,12 +3,10 @@ n = int(input())
 total_sum = 0
 messages = 0
 new_message = 0
-for letter in range(1, n):
+for letter in range(0, n):
     total_sum = 0
     messages = 0
     new_letter = input()
     total_sum += ord(new_letter)
     messages = total_sum + key
     print(chr(messages), end="")
-
-
